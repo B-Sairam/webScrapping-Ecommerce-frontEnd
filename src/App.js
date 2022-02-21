1c4d49fc2ed274f1e2a7ecf57228d0524f648a3e
SAIRAM <87373136+B-Sairam@users.noreply.github.com>
2022-02-21 18:52:56 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,65 @@
-import React from "react";
+import React,{useEffect,useContext,useState} from "react";
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import SearchIcon from '@mui/icons-material/Search';
+import axios from 'axios';
+import env from 'react-dotenv'
+import Home from "./components/Home";
+import Beauty from "./components/Beauty";
+import Dress from "./components/Dress";
+import Furnitures from "./components/Furnitures";
+import Laptops from "./components/Laptops";
+import Mobiles from "./components/Mobiles";
+import Shelves from "./components/Shelves";
+import Shoes from "./components/Shoes";
+import Header from "./components/Header";
+import { Link } from "react-router-dom";
+
+export const product_context = React.createContext("");
 
 function App() {
+  
+  const [products,setProducts]=useState([]);
+  let [searchItem,setSearchItem]=useState('')
+    useEffect(()=>{
+      getData()
+    },[])
+ 
+    let getData= async()=>{
+    let res = await axios.get(env.API_URL)
+        setProducts(res.data.products_data); 
+  }
+  let dress_pro = products.dress_product;
+  let beauty_pro = products.beauty_product;
+  let furniture_pro = products.furnitures_product;
+  let laptops_pro = products.laptops_product;
+  let mobile_pro = products.mobiles_product;
+  let shelves_pro = products.shelves_product;
+  let shoes_pro = products.shoes_product
+ 
+
   return <>
-    <h1>Cheking</h1>
+    <Router>
+      <product_context.Provider value={{dress_pro,beauty_pro,furniture_pro,laptops_pro,mobile_pro,shelves_pro,shoes_pro,searchItem}}>
+        <Header/>
+        <div className="container">
+        <div className="search-bar form-group">
+          <input type="text" className="form-control" placeholder="Search products" onChange={(e)=>setSearchItem(e.target.value)} />
+         <button className="search-btn"><SearchIcon/></button>
+        </div>
+          
+    </div>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/beauty" element={<Beauty/>}/>
+        <Route path="/dress" element={<Dress/>} />
+        <Route path="/furniture" element={<Furnitures/>}/>
+        <Route path="/laptops" element={<Laptops/>} />
+        <Route path="/mobiles" element={<Mobiles/>} />
+        <Route path="/shelves" element={<Shelves/>} />
+        <Route path="/shoes" element={<Shoes/>}/>
+      </Routes>
+      </product_context.Provider>
+    </Router>
     </>
   
 }
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React,{useContext,useState} from "react";
+import { product_context } from "../App";
+import SearchIcon from '@mui/icons-material/Search';
+ 
+function Header() {
+  
+  return <>
+    <div className="head">
+        <h1>Style's wear</h1>
+    </div>
+    </>
+
+}
+
+export default Header;
